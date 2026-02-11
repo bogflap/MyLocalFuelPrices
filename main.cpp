ea8835294294cb7495fea3be0b9f2c512fb628e6
Shaun Flann <shaun.flann@gmail.com>
2026-02-11 18:21:34 +0000

    Got the text formatting right

--- main.cpp
+++ main.cpp
@@ -23,31 +23,39 @@ struct displayValue
 
 struct displayValues
 {
-    int maxCity;
-    int maxCounty;
-    int maxE5;
-    int maxE10;
-    int maxB7p;
-    int maxB7s;
+    qsizetype maxCity;
+    qsizetype maxCounty;
+    qsizetype maxE5;
+    qsizetype maxE10;
+    qsizetype maxB7p;
+    qsizetype maxB7s;
     QMultiMap<float, struct displayValue>   values;
 };
 
-struct displayValues    dvs
+void setMaxSize (qsizetype* current, qsizetype newCandidate)
 {
-    0,
-    0,
-    0,
-    0,
-    0,
-    0,
-    QMultiMap<float, struct displayValue>()
-};
+    if (*current < newCandidate)
+    {
+        *current = newCandidate;
+    }
+}
 
 int main(int argc, char *argv[])
 {
     QCoreApplication    a(argc, argv);
     QTextStream         ts(stdout);
     int                 result = 0;
+
+    struct displayValues    dvs
+        {
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            QMultiMap<float, struct displayValue>()
+        };
 
     while (true)
     {
@@ -75,30 +83,72 @@ int main(int argc, char *argv[])
 
             // Now search through the lines looking for ones of interest
             // Note the first line is the contains the column headers
+            // Note we are omitting any line that does not have a valid E5 value
             for (int i = 1; i < fuelFileLines.size(); i++)
             {
                 QStringList columns;
                 columns = fuelFileLines.at(i).split(",", Qt::KeepEmptyParts);
-                if (columns.at(COUNTY_COLUMN) == COUNTY_NAME)
+                if ((columns.at(COUNTY_COLUMN) == COUNTY_NAME)
+                    && (columns.at(E5_COLUMN).length() != 0))
                 {
-                    ts << i
-                       << " "
-                       << columns.at(CITY_COLUMN)
-                       << " "
-                       << columns.at(COUNTY_COLUMN)
-                       << " "
-                       << columns.at(E5_COLUMN)
-                       << " "
-                       << columns.at(E10_COLUMN)
-                       << " "
-                       << columns.at(B7P_COLUMN)
-                       << " "
-                       << columns.at(B7S_COLUMN)
-                       << " "
-                       << Qt::endl;
-                    ts.flush();
+                    displayValue    dv;
+                    float           keyV;
+
+                    dv.city = columns.at(CITY_COLUMN);
+                    dv.county = columns.at(COUNTY_COLUMN);
+                    dv.e5 = columns.at(E5_COLUMN);
+                    dv.e10 = columns.at(E10_COLUMN);
+                    dv.b7p = columns.at(B7P_COLUMN);
+                    dv.b7s = columns.at(B7S_COLUMN);
+
+                    setMaxSize(&(dvs.maxCity), dv.city.length());
+                    setMaxSize(&(dvs.maxCounty), dv.county.length());
+                    setMaxSize(&(dvs.maxE5), dv.e5.length());
+                    setMaxSize(&(dvs.maxE10), dv.e10.length());
+                    setMaxSize(&(dvs.maxB7p), dv.b7p.length());
+                    setMaxSize(&(dvs.maxB7s), dv.b7s.length());
+                    keyV = dv.e5.toFloat();
+
+                    dvs.values.insert(keyV, dv);
                 }
             }
+
+            // Now display the values
+            QMultiMapIterator<float, struct displayValue>   iDp(dvs.values);
+
+            // Note adding in setFieldWidth to zero before outputting spaces as
+            // the spaces gets a field width as previously set
+            while(iDp.hasNext())
+            {
+                iDp.next();
+                ts.setFieldAlignment(QTextStream::AlignLeft);
+                ts.setFieldWidth(dvs.maxCity);
+                ts << iDp.value().city;
+                ts.setFieldWidth(0);
+                ts << " ";
+                ts.setFieldWidth(dvs.maxCounty);
+                ts << iDp.value().county;
+                ts.setFieldWidth(0);
+                ts << " ";
+                ts.setFieldWidth(dvs.maxE5);
+                ts << iDp.value().e5;
+                ts.setFieldWidth(0);
+                ts << " ";
+                ts.setFieldWidth(dvs.maxE10);
+                ts << iDp.value().e10;
+                ts.setFieldWidth(0);
+                ts << " ";
+                ts.setFieldWidth(dvs.maxB7p);
+                ts << iDp.value().b7p;
+                ts.setFieldWidth(0);
+                ts << " ";
+                ts.setFieldWidth(dvs.maxB7s);
+                ts << iDp.value().b7s;
+                ts.setFieldWidth(0);
+                ts << Qt::endl;
+                ts.flush();
+            }
+
             break;
         }
 
